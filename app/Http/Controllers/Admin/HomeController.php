53bc59cffddd1efb6f0cf1c5305fa46fc6068103
blylxy <36830140@qq.com>
2020-04-27 18:54:10 +0800

    test

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     }
     
     public function index(){
-        //return "<br />test page 123"; 
+        //return "<br />test page 123"; aaakieu
         
         return view('admin/index', $this->baseinfo );
     }
